a83fb50f6a34634b0e6cbedaf84cecfdf743284c
Max Schwenk <maschwenk@gmail.com>
2022-03-02 14:23:45 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# jump-to-test-and-back
+## Jump to Test and Back
+
+This extension jumps between a Typescript implementation and its test, and back
---
+++ src/extension.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+const findAndNavigateToFile = (globPattern: string) : void => {
+  vscode.workspace.findFiles(globPattern, null, 1).then((ps: vscode.Uri[]): void => {
+    if (ps.length === 0) {
+      vscode.window.setStatusBarMessage(`jumpToTestAndBack: Unable to find test file using glob ${globPattern}`, 30000);
+      return;  
+    }
+    if (ps.length > 1) {
+      vscode.window.setStatusBarMessage('jumpToTestAndBack: Found multiple results, going with the first', 30000);
+    }  
+
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.file(ps[0].path));
+  });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('extension.jumpToTestAndBack', () => {
+    const fileName = path.basename(vscode.window.activeTextEditor.document.fileName)
+
+    if (fileName.endsWith('.test.ts')) {
+      findAndNavigateToFile(`**/*${fileName.replace('.test.ts', '.ts')}`);
+    } else if (fileName.endsWith('.ts')) {
+      findAndNavigateToFile(`**/*${fileName.replace('.ts', '.test.ts')}`);
+    } else {
+      vscode.window.setStatusBarMessage('jumpToTestAndBack: Cant parse appropiate filename', 3000);
+      return 
+    }    
+  });
+
+  context.subscriptions.push(disposable);
+}
